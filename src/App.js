23854336cd533ec4667e15536f4c35358bf8a5dd
Guilherme A. Melo <guilhermearmelo@gmail.com>
2019-01-12 21:44:11 -0200

    Add React Project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
+
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 120,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    move: {
+        speed: 5
+    }
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
+        <FaceRecognition />*/}
+      </div>
+    );
+  }
+}
+
+export default App;
